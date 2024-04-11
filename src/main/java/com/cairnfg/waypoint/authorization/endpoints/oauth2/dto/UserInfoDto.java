e6c8323d94d5941bb421d39d7e1cb51f9bbdb35c
tstratton2007 <tfstratton2007@gmail.com>
2024-04-11 12:44:32 -0500

    Added the info to the auth/me resposne also

--- src/main/java/com/cairnfg/waypoint/authorization/endpoints/oauth2/dto/UserInfoDto.java
+++ src/main/java/com/cairnfg/waypoint/authorization/endpoints/oauth2/dto/UserInfoDto.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Data;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Data
@@ -13,8 +14,11 @@ public class UserInfoDto {
 
   @JsonIgnore
   public static final UserInfoDto.UserInfoDtoMapper MAPPER = UserInfoDto.UserInfoDtoMapper.INSTANCE;
+
+  private Long accountId;
   private String username;
   private String firstName;
+  private String lastName;
   private String email;
   private Boolean acceptedTC; //terms and conditions
   private Boolean acceptedEULA; //end-user licensing agreement
@@ -25,7 +29,7 @@ public class UserInfoDto {
 
     UserInfoDto.UserInfoDtoMapper INSTANCE = Mappers.getMapper(UserInfoDto.UserInfoDtoMapper.class);
 
-
+    @Mapping(source = "id", target = "accountId")
     UserInfoDto accountToUserInfoDto(Account account);
   }
 }
